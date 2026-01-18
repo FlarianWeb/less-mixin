23969d727c6825181fea14dd3b99583e1c2045f8
Ramil Musin <musin@flarian.ru>
2026-01-18 05:34:22 +0300

    fix(flexbox): add flexbox category
    add flexbox category to test util

--- tests/run-tests.js
+++ tests/run-tests.js
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const testsDir = __dirname;
-const categories = ['layout'];
+const categories = ['layout', 'flexbox'];
 
 let totalTests = 0;
 let passedTests = 0;
